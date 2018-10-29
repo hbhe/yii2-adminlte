75e6bbd5b93a9e4fcbee74f77230aa754ffcef25
jack <156870057@qq.com>
2018-10-29 15:21:46 +0800

    add

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -7,8 +7,8 @@ Yii::setAlias('@storage', dirname(dirname(__DIR__)) . '/storage');
 Yii::setAlias('@rest', dirname(dirname(__DIR__)) . '/rest');
 Yii::setAlias('@wap', dirname(dirname(__DIR__)) . '/wap');
 
-Yii::setAlias('@frontendUrl', YII_ENV_DEV ? 'http://127.0.0.1/yii2-adminlte/frontend/web/' : 'http://yii2-adminlte-frontend.mitoto.cn');
-Yii::setAlias('@backendUrl', YII_ENV_DEV ? 'http://127.0.0.1/yii2-adminlte/backend/web/' : 'http://yii2-adminlte-backend.mitoto.cn');
-Yii::setAlias('@storageUrl', YII_ENV_DEV ? 'http://127.0.0.1/yii2-adminlte/storage/web/' : 'http://yii2-adminlte-storage.mitoto.cn');
-Yii::setAlias('@restUrl', YII_ENV_DEV ? 'http://127.0.0.1/yii2-adminlte/rest/web/' : 'http://yii2-adminlte-rest.mitoto.cn');
-Yii::setAlias('@wapUrl', YII_ENV_DEV ? 'http://127.0.0.1/yii2-adminlte/wap/web/' : 'http://yii2-adminlte-wap.mitoto.cn');
+Yii::setAlias('@frontendUrl', YII_ENV_DEV ? 'http://127.0.0.1/yii2-adminlte/frontend/web/' : 'http://yii2-adminlte-frontend.9tui.cn');
+Yii::setAlias('@backendUrl', YII_ENV_DEV ? 'http://127.0.0.1/yii2-adminlte/backend/web/' : 'http://yii2-adminlte-backend.9tui.cn');
+Yii::setAlias('@storageUrl', YII_ENV_DEV ? 'http://127.0.0.1/yii2-adminlte/storage/web/' : 'http://yii2-adminlte-storage.9tui.cn');
+Yii::setAlias('@restUrl', YII_ENV_DEV ? 'http://127.0.0.1/yii2-adminlte/rest/web/' : 'http://yii2-adminlte-rest.9tui.cn');
+Yii::setAlias('@wapUrl', YII_ENV_DEV ? 'http://127.0.0.1/yii2-adminlte/wap/web/' : 'http://yii2-adminlte-wap.9tui.cn');
